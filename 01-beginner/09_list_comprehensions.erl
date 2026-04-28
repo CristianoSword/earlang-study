dbe244880fcf4c4cab7cabe2898fa99c106e03eb
Cristiano Camacho <originalvgo@gmail.com>
2026-04-27 23:42:59 -0300

    Add start/0 function to lesson 09 for easier testing

--- 01-beginner/09_list_comprehensions.erl
+++ 01-beginner/09_list_comprehensions.erl
@@ -1,6 +1,13 @@
 %% @doc Lesson 9: List Comprehensions
 -module('09_list_comprehensions').
--export([double/1, even/1, product/2]).
+-export([double/1, even/1, product/2, start/0]).
+
+start() ->
+    L = [1, 2, 3, 4, 5, 6],
+    io:format("Original: ~p~n", [L]),
+    io:format("Doubled: ~p~n", [double(L)]),
+    io:format("Even only: ~p~n", [even(L)]),
+    io:format("Product [1,2] x [a,b]: ~p~n", [product([1, 2], [a, b])]).
 
 double(L) -> [X * 2 || X <- L].
 
