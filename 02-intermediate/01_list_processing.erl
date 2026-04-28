dee1c68deeb305efc61033838d937c2edd215205
Cristiano Camacho <originalvgo@gmail.com>
2026-04-28 00:09:19 -0300

    Add intermediate lesson 01: List Processing

---
+++ 02-intermediate/01_list_processing.erl
@@ -0,0 +1,10 @@
+%% @doc Intermediate 01: Advanced List Processing
+-module('01_list_processing').
+-export([demo/0]).
+
+demo() ->
+    L = [1, 2, 3, 4, 5],
+    Mapped = lists:map(fun(X) -> X * 10 end, L),
+    Filtered = lists:filter(fun(X) -> X rem 2 == 0 end, L),
+    Folded = lists:foldl(fun(X, Acc) -> X + Acc end, 0, L),
+    {Mapped, Filtered, Folded}.
