ff9dec2e71421cff5e3ad869ab73545932620d77
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:46:43 -0300

    Add template for lesson 34: Global Registry

---
+++ 03-advanced/34_global_registry.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 34: Global Registry
+%% Registering process names globally across an Erlang cluster.
+-module('34_global_registry').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 34: Global Registry initialized.~n").
