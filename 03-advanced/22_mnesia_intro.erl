0d3764dc8ef90e1d800393a511c2aaaa261682b1
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:42:57 -0300

    Add template for lesson 22: Mnesia Intro

---
+++ 03-advanced/22_mnesia_intro.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 22: Mnesia Intro
+%% Mnesia is a distributed, soft real-time database management system.
+-module('22_mnesia_intro').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 22: Mnesia Intro initialized.~n").
