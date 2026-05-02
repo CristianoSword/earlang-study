940913b2f35635328233af1b26d7b223acf200f8
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:46:15 -0300

    Populate lesson 32: Custom Behaviors implementation

--- 03-advanced/32_custom_behaviors.erl
+++ 03-advanced/32_custom_behaviors.erl
@@ -1,7 +1,15 @@
 %% @doc Advanced 32: Custom Behaviors
 %% Defining custom behavior specifications in Erlang.
 -module('32_custom_behaviors').
--export([start/0]).
+-export([start/0, callback_mode/0]).
+
+%% Define the callbacks required for this behavior
+-callback init(Args :: term()) -> {ok, State :: term()} | {error, Reason :: term()}.
+-callback handle_message(Msg :: term(), State :: term()) -> {ok, NewState :: term()}.
 
 start() ->
-    io:format("Lesson 32: Custom Behaviors initialized.~n").
+    io:format("Custom behavior defined with callbacks: init/1 and handle_message/2.~n").
+
+%% Implementing part of it for demo
+callback_mode() ->
+    behavior_info(callbacks).
