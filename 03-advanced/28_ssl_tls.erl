6c18d7c33a59f00166d36c8e3c721b3932e47f5f
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:44:44 -0300

    Add template for lesson 28: SSL/TLS

---
+++ 03-advanced/28_ssl_tls.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 28: SSL/TLS
+%% Secure communications over TCP using the ssl module.
+-module('28_ssl_tls').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 28: SSL/TLS initialized.~n").
