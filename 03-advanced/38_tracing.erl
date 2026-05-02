b0d2cbdaeeb3e539de0f3b3014cba7e4bace2d8b
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:48:05 -0300

    Populate lesson 38: Tracing implementation

--- 03-advanced/38_tracing.erl
+++ 03-advanced/38_tracing.erl
@@ -1,7 +1,16 @@
 %% @doc Advanced 38: Tracing (dbg)
 %% Real-time inspection of function calls and messages.
 -module('38_tracing').
--export([start/0]).
+-export([start/0, trace_demo/0, test_fun/1]).
 
 start() ->
-    io:format("Lesson 38: Tracing initialized.~n").
+    trace_demo(),
+    test_fun(hello_tracing).
+
+trace_demo() ->
+    dbg:tracer(),
+    dbg:p(all, c),
+    dbg:tpl(?MODULE, test_fun, cx).
+
+test_fun(Msg) ->
+    io:format("Function called with: ~p~n", [Msg]).
