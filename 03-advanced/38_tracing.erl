accfdd37a6667e2e380b0d30831ea98fb554ae29
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:47:56 -0300

    Add template for lesson 38: Tracing

---
+++ 03-advanced/38_tracing.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 38: Tracing (dbg)
+%% Real-time inspection of function calls and messages.
+-module('38_tracing').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 38: Tracing initialized.~n").
