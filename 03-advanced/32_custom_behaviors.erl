1d9588a5c17facf6ef45f18606fa1f2779147708
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:46:05 -0300

    Add template for lesson 32: Custom Behaviors

---
+++ 03-advanced/32_custom_behaviors.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 32: Custom Behaviors
+%% Defining custom behavior specifications in Erlang.
+-module('32_custom_behaviors').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 32: Custom Behaviors initialized.~n").
