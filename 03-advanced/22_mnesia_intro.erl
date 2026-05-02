c53d940e4767c33989529127649c9405962f0aef
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:43:06 -0300

    Populate lesson 22: Mnesia Intro implementation

--- 03-advanced/22_mnesia_intro.erl
+++ 03-advanced/22_mnesia_intro.erl
@@ -1,7 +1,18 @@
 %% @doc Advanced 22: Mnesia Intro
 %% Mnesia is a distributed, soft real-time database management system.
 -module('22_mnesia_intro').
--export([start/0]).
+-export([start/0, setup/0]).
+
+-record(user, {id, name, role}).
 
 start() ->
-    io:format("Lesson 22: Mnesia Intro initialized.~n").
+    setup(),
+    io:format("Mnesia schema and table 'user' created.~n").
+
+setup() ->
+    mnesia:create_schema([node()]),
+    mnesia:start(),
+    mnesia:create_table(user, [
+        {attributes, record_info(fields, user)},
+        {disc_copies, [node()]}
+    ]).
