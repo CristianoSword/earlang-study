a48d396c87e2e702b0d1b7772a15427012b776e0
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:48:14 -0300

    Add template for lesson 39: Release Management

---
+++ 03-advanced/39_release_management.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 39: Release Management
+%% Packaging Erlang applications for production using relx/rebar3.
+-module('39_release_management').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 39: Release Management initialized.~n").
