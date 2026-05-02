713e10e46e3a5ed3f50fa05f4c01d0c2572f6e84
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:44:00 -0300

    Populate lesson 25: Persistent Term implementation

--- 03-advanced/25_persistent_term.erl
+++ 03-advanced/25_persistent_term.erl
@@ -1,7 +1,15 @@
 %% @doc Advanced 25: Persistent Term
 %% persistent_term is optimized for read-heavy operations on data that changes rarely.
 -module('25_persistent_term').
--export([start/0]).
+-export([start/0, store_config/2, get_config/1]).
 
 start() ->
-    io:format("Lesson 25: Persistent Term initialized.~n").
+    store_config(api_key, "secret_12345"),
+    Val = get_config(api_key),
+    io:format("Config value: ~p~n", [Val]).
+
+store_config(Key, Value) ->
+    persistent_term:put(Key, Value).
+
+get_config(Key) ->
+    persistent_term:get(Key).
