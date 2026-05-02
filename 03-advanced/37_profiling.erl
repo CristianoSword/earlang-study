bdd865aba489e72ded410693ef78ac234e1f0905
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:47:47 -0300

    Populate lesson 37: Profiling implementation

--- 03-advanced/37_profiling.erl
+++ 03-advanced/37_profiling.erl
@@ -1,7 +1,12 @@
 %% @doc Advanced 37: Profiling (fprof)
 %% Identifying performance bottlenecks in Erlang applications.
 -module('37_profiling').
--export([start/0]).
+-export([start/0, profile_me/0]).
 
 start() ->
-    io:format("Lesson 37: Profiling initialized.~n").
+    fprof:apply(?MODULE, profile_me, []),
+    fprof:profile(),
+    fprof:analyse().
+
+profile_me() ->
+    lists:foreach(fun(X) -> X * X end, lists:seq(1, 10000)).
