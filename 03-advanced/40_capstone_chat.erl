af44f25ebb606fe7d05498551de4d96685533079
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:48:34 -0300

    Add template for lesson 40: Capstone Project

---
+++ 03-advanced/40_capstone_chat.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 40: Capstone: Distributed Chat
+%% A final project combining Sockets, Mnesia, and OTP patterns.
+-module('40_capstone_chat').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 40: Distributed Chat initialized.~n").
