2996e4cac933ea19b2b030c38e267ef39a524a5c
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:47:20 -0300

    Add template for lesson 36: Observer

---
+++ 03-advanced/36_observer.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 36: Observer
+%% Using the GUI tool to monitor and troubleshoot Erlang systems.
+-module('36_observer').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 36: Observer initialized.~n").
