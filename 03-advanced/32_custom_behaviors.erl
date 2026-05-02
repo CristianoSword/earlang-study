aa1903fb6dcf133105db489a4f869f78592320dd
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 22:04:08 -0300

    fix 32_custom_behaviors

--- 03-advanced/32_custom_behaviors.erl
+++ 03-advanced/32_custom_behaviors.erl
@@ -1,18 +1,15 @@
 %% @doc Advanced 32: Custom Behaviors
 %% Defining custom behavior specifications in Erlang.
 -module('32_custom_behaviors').
--export([start/0, behaviour_info/1]).
+-export([start/0]).
 
 %% Define the callbacks required for this behaviour (Modern Way)
+%% Note: When using -callback, you MUST NOT define behaviour_info/1 manually.
 -callback init(Args :: term()) -> {ok, State :: term()} | {error, Reason :: term()}.
 -callback handle_message(Msg :: term(), State :: term()) -> {ok, NewState :: term()}.
+
 
 start() ->
     io:format("Custom behaviour defined with callbacks: init/1 and handle_message/2.~n").
 
-%% behaviour_info/1 (British spelling) is the official Erlang standard
-behaviour_info(callbacks) ->
-    [{init, 1}, {handle_message, 2}];
-behaviour_info(_) ->
-    undefined.
 
