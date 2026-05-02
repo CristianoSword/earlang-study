2b42bd94e7f59b23f4e8f1416ada37dd69dc5a12
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:46:34 -0300

    Populate lesson 33: Process Groups implementation

--- 03-advanced/33_process_groups.erl
+++ 03-advanced/33_process_groups.erl
@@ -1,7 +1,14 @@
 %% @doc Advanced 33: Process Groups (pg)
 %% Managing groups of processes across a cluster.
 -module('33_process_groups').
--export([start/0]).
+-export([start/0, demo/0]).
 
 start() ->
-    io:format("Lesson 33: Process Groups initialized.~n").
+    pg:start_link(),
+    demo().
+
+demo() ->
+    GroupName = workers,
+    pg:join(GroupName, self()),
+    Members = pg:get_members(GroupName),
+    io:format("Members of group ~p: ~p~n", [GroupName, Members]).
