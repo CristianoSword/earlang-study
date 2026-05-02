cc3b280cc186f9935e3170cd9be3287c1a05b9ae
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:48:44 -0300

    Populate lesson 40: Capstone Project implementation

--- 03-advanced/40_capstone_chat.erl
+++ 03-advanced/40_capstone_chat.erl
@@ -1,7 +1,21 @@
 %% @doc Advanced 40: Capstone: Distributed Chat
 %% A final project combining Sockets, Mnesia, and OTP patterns.
 -module('40_capstone_chat').
--export([start/0]).
+-export([start/0, broadcast/2, join/1]).
+
+-record(chat_user, {node, name}).
 
 start() ->
-    io:format("Lesson 40: Distributed Chat initialized.~n").
+    mnesia:start(),
+    mnesia:create_table(chat_user, [{attributes, record_info(fields, chat_user)}]),
+    io:format("Distributed Chat System ready.~n").
+
+join(Name) ->
+    mnesia:transaction(fun() -> 
+        mnesia:write(#chat_user{node = node(), name = Name}) 
+    end),
+    io:format("~s joined the chat.~n", [Name]).
+
+broadcast(Sender, Msg) ->
+    {atomic, Users} = mnesia:transaction(fun() -> mnesia:match_object(#chat_user{_ = '_'}) end),
+    [rpc:cast(U#chat_user.node, io, format, ["~s: ~s~n", [Sender, Msg]]) || U <- Users].
