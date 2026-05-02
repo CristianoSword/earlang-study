aafda8cddde349ca4f32ed822d64cb1dba5d2c3f
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:43:15 -0300

    Add template for lesson 23: Mnesia Transactions

---
+++ 03-advanced/23_mnesia_transactions.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 23: Mnesia Transactions
+%% Using transactions ensures ACID properties in Mnesia.
+-module('23_mnesia_transactions').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 23: Mnesia Transactions initialized.~n").
