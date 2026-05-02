da60edbdee8707ca3a3d6a7cd45016a53d8bd0de
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:44:08 -0300

    Add template for lesson 26: Gen_UDP

---
+++ 03-advanced/26_gen_udp.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 26: Gen_UDP
+%% Basic connectionless networking using UDP sockets.
+-module('26_gen_udp').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 26: Gen_UDP initialized.~n").
