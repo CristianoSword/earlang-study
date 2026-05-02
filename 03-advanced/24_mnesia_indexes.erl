fee65cccaa95afeab951b589b7ea082fa35be42d
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:43:33 -0300

    Add template for lesson 24: Mnesia Indexes

---
+++ 03-advanced/24_mnesia_indexes.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 24: Mnesia Indexes
+%% Indexes speed up lookups for non-primary key attributes.
+-module('24_mnesia_indexes').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 24: Mnesia Indexes initialized.~n").
