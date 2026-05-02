01353fa23312d8d7becc8b9c56dcdd0651207c63
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:47:29 -0300

    Populate lesson 36: Observer implementation

--- 03-advanced/36_observer.erl
+++ 03-advanced/36_observer.erl
@@ -1,7 +1,12 @@
 %% @doc Advanced 36: Observer
 %% Using the GUI tool to monitor and troubleshoot Erlang systems.
 -module('36_observer').
--export([start/0]).
+-export([start/0, open_observer/0]).
 
 start() ->
-    io:format("Lesson 36: Observer initialized.~n").
+    open_observer(),
+    io:format("Observer tool opened. Check the GUI for system stats.~n").
+
+open_observer() ->
+    %% This requires the 'runtime_tools' and 'observer' apps to be available
+    observer:start().
