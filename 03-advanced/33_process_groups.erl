da8a40be3f43540cc946f123c12af5ca778aa50d
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:46:24 -0300

    Add template for lesson 33: Process Groups

---
+++ 03-advanced/33_process_groups.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 33: Process Groups (pg)
+%% Managing groups of processes across a cluster.
+-module('33_process_groups').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 33: Process Groups initialized.~n").
