2ed010e1fe94f9332ca6ed97fae0d53f99b9baa6
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:45:46 -0300

    Add template for lesson 31: Dynamic Supervisors

---
+++ 03-advanced/31_dynamic_supervisors.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 31: Dynamic Supervisors
+%% Managing temporary or transient child processes dynamically.
+-module('31_dynamic_supervisors').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 31: Dynamic Supervisors initialized.~n").
