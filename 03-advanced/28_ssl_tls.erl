57edb333439b6bb41db5b987781a7d6baf488da4
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:44:53 -0300

    Populate lesson 28: SSL/TLS implementation

--- 03-advanced/28_ssl_tls.erl
+++ 03-advanced/28_ssl_tls.erl
@@ -1,7 +1,17 @@
 %% @doc Advanced 28: SSL/TLS
 %% Secure communications over TCP using the ssl module.
 -module('28_ssl_tls').
--export([start/0]).
+-export([start/0, fetch_google/0]).
 
 start() ->
-    io:format("Lesson 28: SSL/TLS initialized.~n").
+    ssl:start(),
+    fetch_google().
+
+fetch_google() ->
+    %% Note: In a real scenario, you would use a dedicated HTTP client like 'inets' or 'hackney'
+    %% but here we demonstrate basic ssl:connect
+    {ok, Socket} = ssl:connect("google.com", 443, [binary, {active, false}], 5000),
+    ssl:send(Socket, "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"),
+    {ok, Data} = ssl:recv(Socket, 0, 5000),
+    io:format("SSL Data received: ~p...~n", [binary:part(Data, 0, 50)]),
+    ssl:close(Socket).
