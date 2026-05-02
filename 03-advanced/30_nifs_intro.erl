e39fb772ad809d3619955b6588c13c2cd0ed7ccc
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:45:30 -0300

    Populate lesson 30: NIFs Intro implementation

--- 03-advanced/30_nifs_intro.erl
+++ 03-advanced/30_nifs_intro.erl
@@ -1,7 +1,16 @@
 %% @doc Advanced 30: NIFs Intro
 %% Native Implemented Functions allow calling C code from Erlang.
 -module('30_nifs_intro').
--export([start/0]).
+-export([start/0, add/2, init/0]).
+-on_load(init/0).
 
 start() ->
-    io:format("Lesson 30: NIFs Intro initialized.~n").
+    %% This will likely call the Erlang fallback since we don't have the C library compiled
+    Res = add(10, 20),
+    io:format("Result: ~p~n", [Res]).
+
+init() ->
+    ok = erlang:load_nif("./nif_library", 0).
+
+add(_A, _B) ->
+    erlang:nif_error("NIF library not loaded").
