a33c52abc2edc109f2ff39b370e73b09ce7af759
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:45:11 -0300

    Populate lesson 29: Ports Advanced implementation

--- 03-advanced/29_ports_advanced.erl
+++ 03-advanced/29_ports_advanced.erl
@@ -1,7 +1,19 @@
 %% @doc Advanced 29: Ports Advanced
 %% Complex communication with external OS programs via ports.
 -module('29_ports_advanced').
--export([start/0]).
+-export([start/0, run_port/0]).
 
 start() ->
-    io:format("Lesson 29: Ports Advanced initialized.~n").
+    run_port().
+
+run_port() ->
+    %% On Windows, we'll use 'cmd /c echo' for demonstration
+    Port = open_port({spawn, "cmd /c echo HelloFromPort"}, [binary]),
+    receive
+        {Port, {data, Data}} ->
+            io:format("Port Output: ~p~n", [Data]);
+        {Port, {exit_status, Status}} ->
+            io:format("Port exited with ~p~n", [Status])
+    after 2000 ->
+        io:format("Port timeout~n")
+    end.
