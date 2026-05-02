b894cabe81a4f88468910442589a52cb8781cf8a
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:46:52 -0300

    Populate lesson 34: Global Registry implementation

--- 03-advanced/34_global_registry.erl
+++ 03-advanced/34_global_registry.erl
@@ -1,7 +1,15 @@
 %% @doc Advanced 34: Global Registry
 %% Registering process names globally across an Erlang cluster.
 -module('34_global_registry').
--export([start/0]).
+-export([start/0, register_service/0, find_service/0]).
 
 start() ->
-    io:format("Lesson 34: Global Registry initialized.~n").
+    register_service(),
+    Pid = find_service(),
+    io:format("Global Service found at Pid: ~p~n", [Pid]).
+
+register_service() ->
+    yes = global:register_name(my_global_service, self()).
+
+find_service() ->
+    global:whereis_name(my_global_service).
