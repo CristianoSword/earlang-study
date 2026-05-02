bc58c29db1a64aa25a797e1dafaf39ea0f617804
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:42:49 -0300

    Populate lesson 21: Dets Storage implementation

--- 03-advanced/21_dets_storage.erl
+++ 03-advanced/21_dets_storage.erl
@@ -1,7 +1,25 @@
 %% @doc Advanced 21: Dets (Disk Erlang Term Storage)
 %% Dets is similar to ETS but provides disk-based storage.
 -module('21_dets_storage').
--export([start/0]).
+-export([start/0, demo/0]).
 
 start() ->
-    io:format("Lesson 21: Dets Storage initialized.~n").
+    demo().
+
+demo() ->
+    Name = my_disk_table,
+    File = "my_table.dets",
+    %% Open the table
+    {ok, Name} = dets:open_file(Name, [{file, File}, {type, set}]),
+    
+    %% Insert data
+    dets:insert(Name, {key1, "Hello from Disk"}),
+    dets:insert(Name, {key2, 12345}),
+    
+    %% Lookup
+    {ok, Val} = dets:lookup(Name, key1),
+    io:format("Lookup key1: ~p~n", [Val]),
+    
+    %% Close
+    dets:close(Name),
+    io:format("Dets demo finished.~n").
