6eac2bde5cd77305e4e1c9fb5bf47d87068f72d6
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:45:20 -0300

    Add template for lesson 30: NIFs Intro

---
+++ 03-advanced/30_nifs_intro.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 30: NIFs Intro
+%% Native Implemented Functions allow calling C code from Erlang.
+-module('30_nifs_intro').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 30: NIFs Intro initialized.~n").
