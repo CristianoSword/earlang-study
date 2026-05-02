2d0acf92eb5ef8a58dd8e1effab81aae82bd652a
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:47:01 -0300

    Add template for lesson 35: Distributed Mnesia

---
+++ 03-advanced/35_distributed_mnesia.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 35: Distributed Mnesia
+%% Replicating data across multiple Erlang nodes.
+-module('35_distributed_mnesia').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 35: Distributed Mnesia initialized.~n").
