c6a3b0c3fe2aed4e9389bab2121fdc0a972bf014
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:43:42 -0300

    Populate lesson 24: Mnesia Indexes implementation

--- 03-advanced/24_mnesia_indexes.erl
+++ 03-advanced/24_mnesia_indexes.erl
@@ -1,7 +1,22 @@
 %% @doc Advanced 24: Mnesia Indexes
 %% Indexes speed up lookups for non-primary key attributes.
 -module('24_mnesia_indexes').
--export([start/0]).
+-export([start/0, add_index/0, find_by_role/1]).
+
+-record(user, {id, name, role}).
 
 start() ->
-    io:format("Lesson 24: Mnesia Indexes initialized.~n").
+    mnesia:start(),
+    add_index(),
+    Users = find_by_role(admin),
+    io:format("Admins found: ~p~n", [Users]).
+
+add_index() ->
+    mnesia:add_table_index(user, role).
+
+find_by_role(Role) ->
+    Read = fun() ->
+        mnesia:index_read(user, Role, #user.role)
+    end,
+    {atomic, Results} = mnesia:transaction(Read),
+    Results.
