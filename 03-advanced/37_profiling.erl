61eb8cdaa1fcf162aeee1c1968db4fa7994eabb9
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:47:38 -0300

    Add template for lesson 37: Profiling

---
+++ 03-advanced/37_profiling.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 37: Profiling (fprof)
+%% Identifying performance bottlenecks in Erlang applications.
+-module('37_profiling').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 37: Profiling initialized.~n").
