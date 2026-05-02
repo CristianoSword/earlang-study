16d6fed9df4a6c50fc74705bf055691fde4e7c5d
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:44:26 -0300

    Add template for lesson 27: Gen_TCP

---
+++ 03-advanced/27_gen_tcp.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 27: Gen_TCP
+%% Reliable streaming data over TCP sockets.
+-module('27_gen_tcp').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 27: Gen_TCP initialized.~n").
