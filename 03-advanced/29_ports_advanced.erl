acd551e235ba24b657521bcc81efcab6cabb17ef
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:45:02 -0300

    Add template for lesson 29: Ports Advanced

---
+++ 03-advanced/29_ports_advanced.erl
@@ -0,0 +1,7 @@
+%% @doc Advanced 29: Ports Advanced
+%% Complex communication with external OS programs via ports.
+-module('29_ports_advanced').
+-export([start/0]).
+
+start() ->
+    io:format("Lesson 29: Ports Advanced initialized.~n").
