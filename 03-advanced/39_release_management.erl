a94738ecb58a2197bc577606b5b5a77d5c83cd72
Cristiano Camacho <originalvgo@gmail.com>
2026-05-01 21:48:24 -0300

    Populate lesson 39: Release Management implementation

--- 03-advanced/39_release_management.erl
+++ 03-advanced/39_release_management.erl
@@ -1,7 +1,14 @@
 %% @doc Advanced 39: Release Management
 %% Packaging Erlang applications for production using relx/rebar3.
 -module('39_release_management').
--export([start/0]).
+-export([start/0, info/0]).
 
 start() ->
-    io:format("Lesson 39: Release Management initialized.~n").
+    info().
+
+info() ->
+    io:format("To create a release, add the following to rebar.config:~n"),
+    io:format("{relx, [{release, {myapp, \"0.1.0\"}, [myapp]},~n"),
+    io:format("        {dev_mode, true},~n"),
+    io:format("        {include_erts, false},~n"),
+    io:format("        {extended_start_script, true}]}.~n").
